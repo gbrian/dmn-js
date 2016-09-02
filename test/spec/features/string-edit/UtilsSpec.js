f6731c3a1a7ea349a3599abd62aaa20d28c9c6b2
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-09-02 16:47:34 +0200

    provide string parse function

---
+++ test/spec/features/string-edit/UtilsSpec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var utils = require('../../../../lib/features/string-edit/utils');
+
+describe.only('features/string-edit/utils', function() {
+  describe('parseString', function() {
+    describe('empty', function() {
+      it('should recognize an empty string', function() {
+        var result = utils.parseString('');
+        expect(result).to.eql({type: ''});
+      });
+      it('should recognize a string only containing whitespace', function() {
+        var result = utils.parseString('  ');
+        expect(result).to.eql({type: ''});
+      });
+    });
+
+    describe('disjunction', function() {
+      it('should recognize a simple string as disjunction', function() {
+        var result = utils.parseString('"foo"');
+        expect(result).to.eql({type: 'disjunction', values: ['foo']});
+      });
+      it('should not recognize a simple string without quotation marks as disjunction', function() {
+        var result = utils.parseString('foo');
+        expect(result).to.be.undefined;
+      });
+      it('should recognize list of strings as disjunction', function() {
+        var result = utils.parseString('"foo", "bar", "baz"');
+        expect(result).to.eql({type: 'disjunction', values: ['foo', 'bar', 'baz']});
+      });
+      it('should not recognize a list of strings that contain an expression as disjunction', function() {
+        var result = utils.parseString('"foo", "bar", baz');
+        expect(result).to.be.undefined;
+      });
+      it('should ignore leading and trailing whitespace', function() {
+        var result = utils.parseString('  "foo", "bar", "baz"  ');
+        expect(result).to.eql({type: 'disjunction', values: ['foo', 'bar', 'baz']});
+      });
+    });
+  });
+});
